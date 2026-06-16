f3afa4a752bf3effcfd9be5e11943302502d9d97
Jitender31Singh <jitenderscool@gmail.com>
2026-06-16 11:54:56 +0530

    established communication btw extension and webapp

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -8,6 +8,7 @@
  */
 
 const BASE = window.QA_API_BASE || 'http://localhost:8088';
+// const BASE = window.QA_API_BASE || 'http://3.7.136.248:8088';
 
 function getToken() {
     return localStorage.getItem('qa_token') || '';
