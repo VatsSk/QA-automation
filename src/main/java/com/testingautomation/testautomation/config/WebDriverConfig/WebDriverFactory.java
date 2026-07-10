40a922f8907dbd62c69ea83fc686579b9bf9569c
Saket Jha <saketvats1909@gmail.com>
2026-07-10 09:10:04 +0530

    did refactoring in driver

--- src/main/java/com/testingautomation/testautomation/config/WebDriverConfig/WebDriverFactory.java
+++ src/main/java/com/testingautomation/testautomation/config/WebDriverConfig/WebDriverFactory.java
@@ -7,8 +7,13 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class WebDriverFactory {
@@ -26,7 +31,34 @@ public class WebDriverFactory {
 
         ChromeOptions options = new ChromeOptions();
 
+        // Disable browser-level popups
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        prefs.put("autofill.profile_enabled", false);
+        prefs.put("autofill.credit_card_enabled", false);
+
+        Path profile = null;
+        try {
+            profile = Files.createTempDirectory("selenium-profile");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        options.addArguments("--user-data-dir=" + profile.toString());
+
+        options.setExperimentalOption("prefs", prefs);
+
+        // General browser arguments
         options.addArguments("--disable-gpu");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-save-password-bubble");
+
+        // Accept insecure certificates
+        options.setAcceptInsecureCerts(true);
 
         if (headless) {
             options.addArguments("--headless=new");
@@ -46,7 +78,6 @@ public class WebDriverFactory {
                     options
             );
 
-            // Maximize only when not headless
             if (!headless) {
                 driver.manage().window().maximize();
             }
@@ -56,7 +87,6 @@ public class WebDriverFactory {
 
         WebDriver driver = new ChromeDriver(options);
 
-        // Maximize only when not headless
         if (!headless) {
             driver.manage().window().maximize();
         }
